e4fc0388869a407cfc17a53b07fb5176602bd42d
Antony Jacob <antony.jacob@wavetronix.com>
2026-09-10 15:53:17 +0530

    Inital Menu made

---
+++ LenConverter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int i=1,inp;
+	float km,foot;
+	while (true){
+		cout<<"\n================================\n";
+		cout<<"1. Meters to Kilometers\n2. Meters to Centimeters\n3. Meters to Millimeters\n4. Meters to Feet\n5. Meters to Inches\n6. Exit\n";
+		cout<<"================================\n";
+		cout<<"Enter choice: ";
+		cin>>i;
+	
+	}
+	return 0;
+}
