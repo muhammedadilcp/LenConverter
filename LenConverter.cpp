5d41724ab4a7139ae314fff9d703cb11e402373d
Chris Joseph <chris.joseph@wavetronix.com>
2026-09-20 12:01:33 +0530

    feat:exit feature

--- LenConverter.cpp
+++ LenConverter.cpp
@@ -1,57 +1,74 @@
-#include <iostream>
-using namespace std;
-int main(){
-	int i=1,inp;
-	float km,foot;
-	while (i!=6){
-		cout<<"\n================================\n";
-		cout<<"1. Meters to Kilometers\n2. Meters to Centimeters\n3. Meters to Millimeters\n4. Meters to Feet\n5. Meters to Inches\n6. Exit\n";
-		cout<<"================================\n";
-		cout<<"Enter choice: ";
-		cin>>i;
-		double len;
-		double res;
-		switch(i){
-			case 5:
-				cout<<"Enter the length: ";
-				cin>>len;
-				res=len*39.37;
-				cout<<"Result: "<<res;
-				break;
-			case 1 :
-			
-			        cout<<"\nEnter the value:\n";
-					cin>>len;
-					res = len/1000.0;
-					cout<<"The converted value is:"<<res;
-					break;
-			
-			case 2:
-				cout<<"Enter length in meters";
-				cin>>len;
-				
-				res=len*100;
-				
-				cout<<"Length in centimeters:"<<res<<endl;
-				break;
-				
-			case 3:
-					
-					cout<<"give the value to be converted: "<<endl;
-					cin>>len;
-					
-					res = len * 1000;
-					cout<<"final answer after converting to millimeters = "<<res;
-					break;
-			
-			case 4:
-	                   cout<<"Enter the length(in metres): ";
-	                   cin>>len;
-					   foot= len/30;
-					   cout<<"Length in foot is: "<<foot<<"\n";
-					   break;
-		}
-	}
 
-	return 0;
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int choice;
+    double meters, result;
+
+    do {
+        cout << "\n========================================\n";
+        cout << "       METERS CONVERSION PROGRAM\n";
+        cout << "========================================\n";
+        cout << "1. Meters to Kilometers\n";
+        cout << "2. Meters to Centimeters\n";
+        cout << "3. Meters to Millimeters\n";
+        cout << "4. Meters to Feet\n";
+        cout << "5. Meters to Inches\n";
+        cout << "6. Exit\n";
+        cout << "========================================\n";
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice >= 1 && choice <= 5) {
+            cout << "Enter length in meters: ";
+            cin >> meters;
+
+            if (meters < 0) {
+                cout << "Length cannot be negative!\n";
+                continue;
+            }
+        }
+
+        cout << fixed << setprecision(2);
+
+        switch (choice) {
+            case 1:
+                result = meters / 1000.0;
+                cout << "Result: " << result << " kilometers\n";
+                break;
+
+            case 2:
+                result = meters * 100;
+                cout << "Result: " << result << " centimeters\n";
+                break;
+
+            case 3:
+                result = meters * 1000;
+                cout << "Result: " << result << " millimeters\n";
+                break;
+
+            case 4:
+                result = meters * 3.28084;
+                cout << "Result: " << result << " feet\n";
+                break;
+
+            case 5:
+                result = meters * 39.3701;
+                cout << "Result: " << result << " inches\n";
+                break;
+
+            case 6:
+                cout << "Thank you for using the program!\n";
+                break;
+
+            default:
+                cout << "Invalid choice! Please try again.\n";
+        }
+
+    } while (choice != 6);
+
+    return 0;
 }
